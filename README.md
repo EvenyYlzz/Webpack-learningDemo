3116cc854bba5bf1c4ae4f85505bce21c04a84c8
EvenyYlzz <172463074@qq.com>
2021-08-08 18:20:06 +0800

    feat: 更新文档以及学习完循环依赖问题、解决循环依赖问题 2.4

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Webpack--learningDemo
 
 《Webpack实战（入门、进阶与调优）》同步练习项目
+
+# 运行case文件中的示例代码
+
+- 1.可以在每个case文件夹中的index.html文件右键用Live Server运行，然后在打开的打印控制台中查看结果
+- 2.也可以直接在每个case文件夹中用终端直接node index.js文件查看结果（推荐使用这种）
+- 3.尽量使用第二种，因为使用第一种可能会遇到奇怪的错误
---
+++ test-2/case06(Loop)/bar.js
@@ -0,0 +1,9 @@
+// CommonJs
+const foo = require('./foo.js');
+console.log('value of foo:', foo);
+module.exports = 'This is bar.js';
+
+// ES6
+// import foo from './foo.js';
+// console.log('value of foo:', foo);
+// export default 'This is bar.js'
